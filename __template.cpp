f598b29e9a63f35ab22fae6e4d4a94e6fc5ed2bb
mmrz(from ubuntu24) <univ.t1c2ha3@gmail.com>
2026-07-03 09:11:33 +0900

    add template.cpp

---
+++ __template.cpp
@@ -0,0 +1,16 @@
+#include "./cp_templates/template/template.hpp"
+#include "./cp_templates/template/debug.hpp"
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+
+using namespace mmrz;
+
+void SOLVE(){
+
+}
+
+void mmrz::solve(){
+        int t = 1;
+        //cin >> t;
+        while(t--)SOLVE();
+}
+
